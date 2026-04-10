8b525add432b3208d7d60d5eb870f51a07278685
Kishor <kishoranbu05@gmail.com>
2026-04-10 15:03:11 +0530

    fixed sidebar

--- app/Views/partials/sidebar.php
+++ app/Views/partials/sidebar.php
@@ -11,15 +11,7 @@ $canProforma = can('billable_items.view') || can('client_masters.view');
 <aside class="app-sidebar">
     <div class="app-panel app-panel--sidebar">
         <div class="sidebar-top px-3 pt-3 pb-2 d-flex align-items-center justify-content-between gap-2 app-brand">
-            <!-- <button type="button" class="btn p-0 border-0 btn-sidebar-toggle" id="btnToggleSidebar" onclick="toggleSidebar()">
-                <span class="nav-ico">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <line x1="3" y1="12" x2="21" y2="12"></line>
-                        <line x1="3" y1="6" x2="21" y2="6"></line>
-                        <line x1="3" y1="18" x2="21" y2="18"></line>
-                    </svg>
-                </span>
-            </button> -->
+
             <div class="brand-logo-wrap flex-grow-1">
                 <img class="brand-logo" src="<?= base_url('assets/img/Netkathir_logo.png') ?>" alt="Netkathir Logo">
             </div>
